4b682e0881d92b41fb2a359e0ebfb099a3ab4c48
Willem <willemhumphrey@gmail.com>
2021-12-16 15:48:19 -0800

    init commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const { ApolloServer } = require('apollo-server');
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Post {
+    id: ID!
+    username: String!
+  }
+
+  type Query {
+    sayHi: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    sayHi: () => 'hello world',
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server
+  .listen({ port: 5000 })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
